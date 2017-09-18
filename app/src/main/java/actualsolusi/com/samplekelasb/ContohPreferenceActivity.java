454f831b977111ea07e189a810aced8ba94e1d4e
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-18 12:43:45 +0700

    update

---
+++ app/src/main/java/actualsolusi/com/samplekelasb/ContohPreferenceActivity.java
@@ -0,0 +1,13 @@
+package actualsolusi.com.samplekelasb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContohPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contoh_preference);
+    }
+}
