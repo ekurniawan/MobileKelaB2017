58df67b02709c636ec01957bace96a61d2cfa162
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-25 12:16:07 +0700

    menambahkan menu

--- app/src/main/java/actualsolusi/com/samplekelasb/MainActivity.java
+++ app/src/main/java/actualsolusi/com/samplekelasb/MainActivity.java
@@ -3,6 +3,8 @@ package actualsolusi.com.samplekelasb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,5 +46,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentParsing);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuCalc:
+                Intent intentCalc = new Intent(MainActivity.this,
+                        CalculatorActivity.class);
+                startActivity(intentCalc);
+                return true;
+            case R.id.menuPref:
+                Intent intentPref = new Intent(MainActivity.this,
+                        MyPreferencesActivity.class);
+                startActivity(intentPref);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
