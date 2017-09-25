f4ca5eaffefdb06248c3ac8e58715b1eff2a6d66
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-25 12:41:53 +0700

    menambahkan recycler view

---
+++ app/src/main/java/actualsolusi/com/samplekelasb/SampleRecyclerView.java
@@ -0,0 +1,13 @@
+package actualsolusi.com.samplekelasb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_recycler_view);
+    }
+}
