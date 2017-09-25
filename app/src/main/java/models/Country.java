54150e4af6a259822166703b7647a6d360d43073
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-25 12:57:38 +0700

    menambahkan recycler view

---
+++ app/src/main/java/models/Country.java
@@ -0,0 +1,26 @@
+package models;
+
+/**
+ * Created by erick on 25/09/2017.
+ */
+
+public class Country {
+    private String name;
+    private double population;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(double population) {
+        this.population = population;
+    }
+}
