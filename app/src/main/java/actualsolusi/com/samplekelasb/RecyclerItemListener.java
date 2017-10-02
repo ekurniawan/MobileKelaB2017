8d386628db3ec38bc36cc374c9a1d4f8e759d997
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-02 12:26:51 +0700

    menambahkan recycler view

--- app/src/main/java/actualsolusi/com/samplekelasb/RecyclerItemListener.java
+++ app/src/main/java/actualsolusi/com/samplekelasb/RecyclerItemListener.java
@@ -1,6 +1,8 @@
 package actualsolusi.com.samplekelasb;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -11,10 +13,30 @@ import android.view.View;
 public class RecyclerItemListener implements RecyclerView.OnItemTouchListener {
 
     private RecyclerTouchListener listener;
+    private GestureDetector gd;
 
     public interface RecyclerTouchListener{
         public void OnClickItem(View v, int position);
         public void OnLongClickItem(View v,int position);
+    }
+
+    public RecyclerItemListener(Context ctx, final RecyclerView rv,
+                                final RecyclerTouchListener listener){
+        this.listener = listener;
+        gd = new GestureDetector(ctx,new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public void onLongPress(MotionEvent e) {
+                View v = rv.findChildViewUnder(e.getX(),e.getY());
+                listener.OnLongClickItem(v,rv.getChildAdapterPosition(v));
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                View v = rv.findChildViewUnder(e.getX(),e.getY());
+                listener.OnClickItem(v,rv.getChildAdapterPosition(v));
+                return true;
+            }
+        });
     }
 
     @Override
