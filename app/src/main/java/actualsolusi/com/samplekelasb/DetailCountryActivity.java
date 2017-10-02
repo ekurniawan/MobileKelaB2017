83217df51570e251873f4b4b04f9d638f9f9a3e8
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-02 13:10:20 +0700

    menambahkan recycler view

---
+++ app/src/main/java/actualsolusi/com/samplekelasb/DetailCountryActivity.java
@@ -0,0 +1,13 @@
+package actualsolusi.com.samplekelasb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailCountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_country);
+    }
+}
